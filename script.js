26eb6fcc00379e3679ceaba7a96ba1cf3ba67080
pratyush2905 <pratyushdubey2021@gmail.com>
2023-12-28 23:33:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = ["aquamarine", "coral", "fuchsia", "gold", "greenyellow", "indigo", "orchid", "plum"];
+const colorPick = [...colors, ...colors];
+const tileCount = colorPick.length;
+
+///Game State
+let revealedCount = 0;
+let activeTile = null;
+let EndMove = false;
+
+
+function buildTiles(col) {
+    const element = document.createElement("div");
+
+    element.classList.add("tile");
+
+    element.setAttribute("data-color", col);
+    element.setAttribute("data-revelaed", false);
+
+    element.addEventListener("click", () => {
+        const reveal = element.getAttribute("data-revealed");
+
+        if (EndMove || reveal === "true" || element===activeTile) {
+            return;
+        }
+
+        element.style.backgroundColor = col;
+
+        if (!activeTile) {
+            activeTile = element;
+            return;
+        }
+
+        const colorToMatch = activeTile.getAttribute("data-color");
+
+        if (colorToMatch === col) {
+
+            activeTile.setAttribute("data-revealed", true);
+            element.setAttribute("data-revealed", true);
+
+            EndMove = false;
+            activeTile = null;
+            revealedCount += 2;
+
+            if (revealedCount === tileCount) {
+                alert("You Win!!!");
+            }
+            return;
+        }
+
+        EndMove = true;
+        setTimeout(() => {
+
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+
+            EndMove = false;
+            activeTile = null;
+
+        }, 700);
+
+    });
+
+    return element;
+}
+
+for (let i = 0; i < tileCount; i++) {
+    const randomInd = Math.floor(Math.random() * colorPick.length);
+    const col = colorPick[randomInd];
+
+    colorPick.splice(randomInd, 1);
+
+    const tile = buildTiles(col);
+
+    tilesContainer.appendChild(tile);
+}
+
+
+
